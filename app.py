5ca204545676933f8e54b8cca32410e28744a630
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2020-11-30 00:06:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,redirect,request,url_for
+import re
+import feedparser
+#from parser import *
+
+app=Flask(__name__)
+
+rss_url= 'https://anchor.fm/s/3c55a5f0/podcast/rss'
+
+titles=[]
+descs=[]
+links=[]
+
+@app.route('/')
+def index():
+
+    def cleanhtml(raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+
+    parser= feedparser.parse(rss_url)
+
+    for entry in parser.entries:
+
+        title=entry.title
+        titles.append(title)
+
+        #generating iframe src
+
+        src=entry.links[0].href
+        indices = [0,25,len(src)]
+        parts = [src[i:j] for i,j in zip(indices, indices[1:]+[None])]
+        iframe_src=parts[0]+"/embed"+parts[1]
+        links.append(iframe_src)
+        desc=cleanhtml(entry.content[0].value)
+        descs.append(desc)
+    print(len(links))
+    print(links[0])
+    return render_template('index.html',len=len(links),descs=descs,links=links,titles=titles)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True) 

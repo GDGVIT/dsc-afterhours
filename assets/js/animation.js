3631f5c533a10f52be5b1d98ca03152a1ffa0e29
batman004 <pathania.yuvraj@gmail.com>
2020-12-01 14:43:11 +0530

    changes

--- assets/js/animation.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('animated-icon'),
-  renderer: 'svg',
-  loop: false,
-  autoplay: true,
-  path: './assets/js/icon.json'
-})

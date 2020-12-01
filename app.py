c34ab08f8d22a28ce1f1357804b51d93dad81439
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2020-12-01 14:52:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,26 +24,24 @@ def index():
     parser= feedparser.parse(rss_url)
     cnt=0
     for entry in parser.entries:
-        if(cnt<1):
-            title=entry.title
-            titles.append(title)
-            date=entry.published[5:16]
-            dates.append(date)
-            #generating iframe src
+    
+        title=entry.title
+        titles.append(title)
+        date=entry.published[5:16]
+        dates.append(date)
+        #generating iframe src
 
-            src=entry.links[0].href
-            indices = [0,25,len(src)]
-            parts = [src[i:j] for i,j in zip(indices, indices[1:]+[None])]
-            iframe_src=parts[0]+"/embed"+parts[1]
-            links.append(iframe_src)
-            desc=cleanhtml(entry.content[0].value)
-            descs.append(desc)
-        cnt+=cnt
+        src=entry.links[0].href
+        indices = [0,25,len(src)]
+        parts = [src[i:j] for i,j in zip(indices, indices[1:]+[None])]
+        iframe_src=parts[0]+"/embed"+parts[1]
+        links.append(iframe_src)
+        desc=cleanhtml(entry.content[0].value)
+        descs.append(desc)
+    
     linkss=set(links)  
     linksss=list(linkss)
-    for i in descs:
-        print(i)
-        print()  
+    print(linksss)  
     return render_template('index.html',len=len(linksss),descs=descs,links=links,titles=titles,dates=dates,cnt=cnt)
 
 

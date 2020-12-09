dcd78c3ed737c0bf05367214850ec2e8cf9fd709
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-12-09 22:38:45 +0530

    rebased and merge conflicts resolved

---
+++ assets/js/animation.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animated-icon'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: "./assets/js/icon.json"
+})
